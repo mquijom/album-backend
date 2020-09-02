4d649094b56c698840991992350e1450d759caf5
Mark Jhon Paul Quijom <mquijom@centralizedinc.com>
2020-09-02 08:42:12 +0800

    include albums & add gitignore

--- routes/photos.js
+++ routes/photos.js
@@ -84,27 +84,30 @@ router.route('/list')
   .post(async (req, res) => {
     let files = await getFiles('../albums');
     let actions = [], albums = []
-    files.data.forEach((album) => {
-      albums.push({
-        album,
-        _path: `../albums/${album}`
+    if (files && files.data)
+      files.data.forEach((album) => {
+        albums.push({
+          album,
+          _path: `../albums/${album}`
+        })
+        actions.push(getFiles(`../albums/${album}`))
       })
-      actions.push(getFiles(`../albums/${album}`))
-    })
     let images = await Promise.all(actions)
     let documents = [];
-    albums.forEach(({ album, _path }) => {
-      let data = images.find(v => v.path === _path);
-      data.data.forEach((name, i) => {
-        documents.push({
-          id: `${album}_${i}`,
-          album,
-          name,
-          path: `/albums/${album}/${name}`,
-          raw: `${host}/photos/${album}/${name}`
-        })
+    if (images && images.length)
+      albums.forEach(({ album, _path }) => {
+        let data = images.find(v => v.path === _path);
+        if (data && data.data)
+          data.data.forEach((name, i) => {
+            documents.push({
+              id: `${album}_${i}`,
+              album,
+              name,
+              path: `/albums/${album}/${name}`,
+              raw: `${host}/photos/${album}/${name}`
+            })
+          })
       })
-    })
     let skip = req.body.skip || 0;
     let limit = req.body.limit || 10;
     let sliced_documents = documents.slice(skip, (skip + limit))
